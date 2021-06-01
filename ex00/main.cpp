c2ee57e1d350a935341558415f8d40ad988478f1
solareenlo <solareenlo@gmail.com>
2021-06-02 08:55:41 +0900

    fix: ex00/main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/02 08:28:06 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/06/02 08:45:53 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/06/02 08:55:11 by tayamamo         ###   ########.fr       */
 /*   Copyright 2021                                                           */
 /* ************************************************************************** */
 
@@ -30,6 +30,8 @@ void    _test(std::string const& name, int grade) {
         std::cout << bure;
         bure.decrementGrade();
         std::cout << bure;
+        bure.decrementGrade();
+        std::cout << bure;
 
         std::cout << std::endl;
     }
@@ -42,9 +44,9 @@ void    _test(std::string const& name, int grade) {
 int main() {
     _test("Kaneda", 1);
     _test("Tetsuo", 150);
-    _test("Akira", 999);
+    _test("Akira", 100);
     _test("Kiyoko", 150);
-    _test("Takashi", 999);
-    _test("Takashi", 0);
+    _test("Takashi", -999);
+    _test("Masaru", 999);
     return 0;
 }
