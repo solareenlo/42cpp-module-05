583e06720909a4ae3b3efbb647c0a6eba4734a48
solareenlo <solareenlo@gmail.com>
2021-06-01 11:05:33 +0900

    fix: ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 00:09:04 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/06/01 10:28:52 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/06/01 11:01:45 by tayamamo         ###   ########.fr       */
 /*   Copyright 2021                                                           */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include "Bureaucrat.hpp"
 #include "Form.hpp"
 
-void    _test_form(std::string const& name_bure, int grade_bure,
+void    _test(std::string const& name_bure, int grade_bure,
         std::string const& name_form, int grade_sign, int grade_exec) {
     try {
         Bureaucrat  bure(name_bure, grade_bure);
@@ -26,27 +26,29 @@ void    _test_form(std::string const& name_bure, int grade_bure,
         std::cout << form;
     }
     catch (const std::exception& e) {
-        std::cout << e.what() << std::endl;
+        std::cout << "Error: " << e.what() << std::endl;
     }
 }
 
 int main() {
     std::cout << "<TEST FORM>" << std::endl;
-    _test_form("Akira", 10, "test1", 100, 100);
+    _test("Akira", 10, "test1", 100, 100);
     std::cout << std::endl;
-    _test_form("Tetsuo", 100, "test1", 10, 10);
+    _test("Tetsuo", 100, "test1", 10, 10);
     std::cout << std::endl;
-    _test_form("Kiyoko", 100, "test1", -10, 10);
+    _test("Kiyoko", 100, "test1", -10, 10);
     std::cout << std::endl;
-    _test_form("Takashi", 100, "test1", 10, -10);
+    _test("Takashi", 100, "test1", 10, -10);
     std::cout << std::endl;
-    _test_form("Masaru", 100, "test1", 10, 999);
+    _test("Masaru", 100, "test1", 10, 999);
     std::cout << std::endl;
-    _test_form("Taisa", 100, "test1", 999, 10);
+    _test("Taisa", 100, "test1", 999, 10);
     std::cout << std::endl;
-    _test_form("A", 999, "test1", 999, 999);
+    _test("A", 999, "test1", 999, 999);
     std::cout << std::endl;
-    _test_form("B", 100, "test1", -999, -999);
+    _test("B", -999, "test1", 999, 999);
+    std::cout << std::endl;
+    _test("C", 100, "test1", -999, -999);
     std::cout << std::endl;
     return (0);
 }
