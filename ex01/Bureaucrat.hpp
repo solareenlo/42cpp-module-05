106fae88be880251378e2c39a885cf7b3cea5dad
solareenlo <solareenlo@gmail.com>
2021-06-02 09:14:18 +0900

    fix: ex00

---
+++ ex01/Bureaucrat.hpp
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/01 19:32:03 by tayamamo          #+#    #+#             */
+/*   Updated: 2021/06/02 09:07:18 by tayamamo         ###   ########.fr       */
+/*   Copyright 2021                                                           */
+/* ************************************************************************** */
+
+#ifndef EX01_BUREAUCRAT_HPP_
+#define EX01_BUREAUCRAT_HPP_
+
+#include <iostream>
+#include <string>
+#include <exception>
+
+class   Bureaucrat {
+ public:
+    Bureaucrat(std::string const& name, int const grade);
+    Bureaucrat(Bureaucrat const& src);
+    ~Bureaucrat();
+    Bureaucrat&         operator=(Bureaucrat const& right);
+
+    std::string const&  getName() const;
+    int                 getGrade() const;
+    void                incrementGrade();
+    void                decrementGrade();
+
+ private:
+    Bureaucrat();
+
+    std::string const   name_;
+    int                 grade_;
+    void                setGrade(int grade);
+
+    static const int    highest_grade_ = 1;
+    static const int    lowest_grade_ = 150;
+    int                 getHighestGrade() const;
+    int                 getLowestGrade() const;
+
+    void                checkGrade(int grade);
+
+    class   GradeTooHighException : public std::exception {
+     public:
+        GradeTooHighException();
+        GradeTooHighException(GradeTooHighException const& src);
+        ~GradeTooHighException() throw();
+        GradeTooHighException& operator=(GradeTooHighException const& right);
+
+        char const* what() const throw();
+    };
+
+    class   GradeTooLowException : public std::exception {
+     public:
+        GradeTooLowException();
+        GradeTooLowException(GradeTooLowException const& src);
+        ~GradeTooLowException() throw();
+        GradeTooLowException& operator=(GradeTooLowException const& right);
+
+        char const* what() const throw();
+    };
+};
+
+std::ostream&   operator<<(std::ostream& stream, Bureaucrat const& right);
+
+#endif  // EX01_BUREAUCRAT_HPP_
