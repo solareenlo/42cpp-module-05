263aabdfd968f18ced460dc403b5ca889caa5886
solareenlo <solareenlo@gmail.com>
2021-06-02 10:49:16 +0900

    update: ex01

--- ex01/Form.hpp
+++ ex01/Form.hpp
@@ -1,47 +1,49 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/06/01 19:32:03 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/06/02 09:37:53 by tayamamo         ###   ########.fr       */
+/*   Created: 2021/06/02 09:23:48 by tayamamo          #+#    #+#             */
+/*   Updated: 2021/06/02 10:46:17 by tayamamo         ###   ########.fr       */
 /*   Copyright 2021                                                           */
 /* ************************************************************************** */
 
-#ifndef EX01_BUREAUCRAT_HPP_
-#define EX01_BUREAUCRAT_HPP_
+#ifndef EX01_FORM_HPP_
+#define EX01_FORM_HPP_
 
 #include <iostream>
 #include <string>
 #include <exception>
-#include "Form.hpp"
+#include "Bureaucrat.hpp"
 
-class   Form;
+class   Bureaucrat;
 
-class   Bureaucrat {
+class   Form {
  public:
-    Bureaucrat(std::string const& name, int const grade);
-    Bureaucrat(Bureaucrat const& src);
-    ~Bureaucrat();
-    Bureaucrat&         operator=(Bureaucrat const& right);
+    Form(std::string const& name, int signGrade, int execGrade);
+    Form(Form const& src);
+    ~Form();
+    Form&   operator=(Form const& right);
 
     std::string const&  getName() const;
-    int                 getGrade() const;
-    void                incrementGrade();
-    void                decrementGrade();
-    void                signForm(Form* form) const;
+    int                 getSignGrade() const;
+    int                 getExecGrade() const;
+    bool                getSigned() const;
+    void                beSigned(Bureaucrat const& bure);
 
  private:
-    Bureaucrat();
+    Form();
     std::string const   name_;
-    int                 grade_;
+    int                 sign_grade_;
+    int                 exec_grade_;
+    bool                signed_;
     static const int    highest_grade_ = 1;
     static const int    lowest_grade_ = 150;
-    void                setGrade(int grade);
-    int                 getHighestGrade() const;
-    int                 getLowestGrade() const;
+    int                 getHighestGrade_() const;
+    int                 getLowestGrade_() const;
+    void                setSigned(bool sign);
     void                checkGrade(int grade);
 
     class   GradeTooHighException : public std::exception {
@@ -49,7 +51,7 @@ class   Bureaucrat {
         GradeTooHighException();
         GradeTooHighException(GradeTooHighException const& src);
         ~GradeTooHighException() throw();
-        GradeTooHighException& operator=(GradeTooHighException const& right);
+        GradeTooHighException&  operator=(GradeTooHighException const& right);
         char const* what() const throw();
     };
 
@@ -58,11 +60,11 @@ class   Bureaucrat {
         GradeTooLowException();
         GradeTooLowException(GradeTooLowException const& src);
         ~GradeTooLowException() throw();
-        GradeTooLowException& operator=(GradeTooLowException const& right);
+        GradeTooLowException&   operator=(GradeTooLowException const& right);
         char const* what() const throw();
     };
 };
 
-std::ostream&   operator<<(std::ostream& stream, Bureaucrat const& right);
+std::ostream&   operator<<(std::ostream& ostream, Form const& right);
 
-#endif  // EX01_BUREAUCRAT_HPP_
+#endif  // EX01_FORM_HPP_
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/02 08:28:06 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/06/02 08:55:11 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/06/02 10:48:45 by tayamamo         ###   ########.fr       */
 /*   Copyright 2021                                                           */
 /* ************************************************************************** */
 
@@ -14,25 +14,16 @@
 #include <string>
 #include "Bureaucrat.hpp"
 
-void    _test(std::string const& name, int grade) {
+void    _test(std::string const& name_bure, int grade,
+        std::string const& name_form, int sign_grade, int exec_grade) {
     try {
-        std::cout << name << "'s test" << std::endl;
+        std::cout << name_bure << "'s test" << std::endl;
 
-        Bureaucrat  bure(name, grade);
+        Bureaucrat  bure(name_bure, grade);
+        Form        form(name_form, sign_grade, exec_grade);
         std::cout << bure;
-
-        bure.incrementGrade();
-        std::cout << bure;
-        bure.incrementGrade();
-        std::cout << bure;
-
-        bure.decrementGrade();
-        std::cout << bure;
-        bure.decrementGrade();
-        std::cout << bure;
-        bure.decrementGrade();
-        std::cout << bure;
-
+        bure.signForm(&form);
+        std::cout << form;
         std::cout << std::endl;
     }
     catch (std::exception& e) {
@@ -42,11 +33,15 @@ void    _test(std::string const& name, int grade) {
 }
 
 int main() {
-    _test("Kaneda", 1);
-    _test("Tetsuo", 150);
-    _test("Akira", 100);
-    _test("Kiyoko", 150);
-    _test("Takashi", -999);
-    _test("Masaru", 999);
+    std::cout << "<TEST FORM>" << std::endl;
+    _test("Akira", 10, "test1", 100, 100);
+    _test("Tetsuo", 100, "test1", 10, 10);
+    _test("Kiyoko", 100, "test1", -10, 10);
+    _test("Takashi", 100, "test1", 10, -10);
+    _test("Masaru", 100, "test1", 10, 999);
+    _test("Taisa", 100, "test1", 999, 10);
+    _test("A", 999, "test1", 999, 999);
+    _test("B", -999, "test1", 999, 999);
+    _test("C", 100, "test1", -999, -999);
     return 0;
 }
