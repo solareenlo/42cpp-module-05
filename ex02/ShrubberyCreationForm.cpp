953fbd098aacfc232c1d96655926f0bff0c918b4
solareenlo <solareenlo@gmail.com>
2021-06-05 03:36:18 +0900

    add: ShrubberyCreationForm

--- ex02/ShrubberyCreationForm.cpp
+++ ex02/ShrubberyCreationForm.cpp
@@ -6,8 +6,25 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/02 21:13:40 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/06/02 21:15:33 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/06/04 08:52:15 by tayamamo         ###   ########.fr       */
 /*   Copyright 2021                                                           */
 /* ************************************************************************** */
 
 #include "ShrubberyCreationForm.hpp"
+
+ShrubberyCreationForm::ShrubberyCreationForm() : Form("Shrubbery", 145, 137) {}
+
+ShrubberyCreationForm::ShrubberyCreationForm(std::string const& target) {
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(ShrubberyCreationForm const& src)
+    : Form(src) {
+}
+
+ShrubberyCreationForm::~ShrubberyCreationForm() {}
+
+ShrubberyCreationForm&
+ShrubberyCreationForm::operator=(ShrubberyCreationForm const& right) {
+    Form::operator=(right);
+    return (*this);
+}
--- ex02/ShrubberyCreationForm.hpp
+++ ex02/ShrubberyCreationForm.hpp
@@ -6,17 +6,28 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/02 20:35:16 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/06/02 20:38:45 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/06/04 08:50:34 by tayamamo         ###   ########.fr       */
 /*   Copyright 2021                                                           */
 /* ************************************************************************** */
 
 #ifndef EX02_SHRUBBERYCREATIONFORM_HPP_
 #define EX02_SHRUBBERYCREATIONFORM_HPP_
 
+#include <string>
 #include "Form.hpp"
 
 class   ShrubberyCreationForm : public Form {
+ public:
     ShrubberyCreationForm(ShrubberyCreationForm const& src);
+    explicit ShrubberyCreationForm(std::string const& target);
+    ~ShrubberyCreationForm();
+    ShrubberyCreationForm&  operator=(ShrubberyCreationForm const& right);
+
+    void    execute(Bureaucrat const& executor) const;
+
+ private:
+    ShrubberyCreationForm();
+    const char* ascii_tree_;
 };
 
 #endif  // EX02_SHRUBBERYCREATIONFORM_HPP_
